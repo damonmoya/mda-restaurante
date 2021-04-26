5b827bcf349e9eb000c8da3bd5c5e9734669130e
Damon Garcia Garcia <damon99@hotmail.es>
2021-04-26 11:27:30 +0100

    Layout y página principal

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-Route::get('/home', function () {
-    return view('home');
-});
-
+})->name('home');
