ff73f56ecdd611bbeb82f3d6457ef781df22eddb
Damon Garcia Garcia <damon99@hotmail.es>
2021-04-26 15:56:09 +0100

    Nuevos campos de usuario y validaciones

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,6 +25,8 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
+            'address' => $this->faker->address,
+            'phone' => $this->faker->regexify('[0-9]{3} [0-9]{2} [0-9]{2} [0-9]{2}'),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,7 +15,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
+        User::factory()->create([
             'name' => 'Pepe Benavente',
             'email' => 'pepebenavente@hotmail.es',
             'password' => bcrypt('elmejorcantante'),
