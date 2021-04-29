8d88399a7d57ea2d6f003bec88d200b4827a6842
Damon Garcia Garcia <damon99@hotmail.es>
2021-04-30 00:29:10 +0100

    Formulario crear nuevo usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,7 +25,7 @@ class UserController extends Controller
      */
     public function create()
     {
-        return view('users.new');
+        return view('users.create');
     }
 
     /**
@@ -36,7 +36,46 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = request()->validate([
+            'name' => ['required', 'min:2', 'max:50'],
+            'surname' => ['required', 'min:10', 'max:50'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => ['required', 'min:6'],
+            'confirm_password' => ['required', 'same:password'],
+            'address' => 'required',
+            'postalCode' => ['required', 'regex:/[0-9]{5}/'],
+            'phone' => ['required', 'regex:/[0-9]{3} [0-9]{2} [0-9]{2} [0-9]{2}/']
+        ], [
+            'name.required' => 'El campo nombre es obligatorio',
+            'name.min' => 'El nombre debe tener más de 2 caracteres',
+            'name.max' => 'El nombre debe tener menos de 50 caracteres',
+            'surname.required' => 'El campo apellidos es obligatorio',
+            'surname.min' => 'Los apellidos deben tener más de 10 caracteres',
+            'surname.max' => 'Los apellidos deben tener menos de 50 caracteres',
+            'email.required' => 'El campo correo es obligatorio',
+            'email.email' => 'El correo no es válido',
+            'email.unique' => 'El correo ya está en uso',
+            'password.required' => 'El campo clave es obligatorio',
+            'password.min' => 'La clave debe tener mínimo 6 caracteres',
+            'confirm_password.required' => 'Se debe confirmar la clave',
+            'confirm_password.same' => 'Las claves no coinciden',
+            'address.required' => 'El campo dirección es obligatorio',
+            'phone.required' => 'El campo teléfono es obligatorio',
+            'phone.regex' => 'El teléfono introducido no es válido',
+            'postalCode.regex' => 'El código postal introducido no es válido'
+        ]);
+
+        $user = User::create([
+            'name' => $data['name'],
+            'surname' => $data['surname'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+            'address' => $data['address'],
+            'postalCode' => $data['postalCode'],
+            'phone' => $data['phone']
+        ]);
+
+        return redirect()->route('users.index');
     }
 
     /**
---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,85 @@
+@extends('layout')
+
+@section('title', "Crear nuevo usuario")
+
+@section('content')
+
+    <h2>Crear nuevo usuario</h2>
+
+    {{--Sección de errores--}}
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <h5>Error en la creación del usuario:</h5>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error}}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    {{--Sección de formulario--}}
+    <form method="POST" action="{{ route('register_send') }}">
+        {{ csrf_field() }}
+        <div class="row">
+            <div class="col-4">
+                <div class="form-group">
+                    <label for="name">Nombre</label>
+                    <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" required>
+                </div>
+            </div>
+            <div class="col-8">
+                <div class="form-group">
+                    <label for="surname">Apellidos</label>
+                    <input type="text" class="form-control" id="surname" name="surname" value="{{ old('surname') }}" required>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-8">
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" required>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="form-group">
+                    <label for="phone">Teléfono</label>
+                    <input type="text" class="form-control" id="phone" name="phone" value="{{ old('phone') }}" required>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-8">
+                <div class="form-group">
+                    <label for="address">Dirección</label>
+                    <input type="text" class="form-control" id="address" name="address" value="{{ old('address') }}" required>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="form-group">
+                    <label for="postalCode">Código postal</label>
+                    <input type="text" class="form-control" id="postalCode" name="postalCode" value="{{ old('postalCode') }}" required>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="form-group">
+            <label for="password">Clave:</label>
+            <input type="password" class="form-control" id="password" name="password" required>
+        </div>
+
+        <div class="form-group">
+            <label for="confirm_password">Confirmar clave:</label>
+            <input type="password" class="form-control" id="confirm_password" name="confirm_password" required>
+        </div>
+
+        <div class="form-group">
+            <button style="cursor:pointer" type="submit" class="btn btn-primary">Confirmar</button>
+        </div>
+    </form>
+
+@endsection
