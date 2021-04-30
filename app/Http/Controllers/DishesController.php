074cf6b1f34b66bfbff1e77a1a356443fc50e071
Daniel Martinez <danielmm1024@gmail.com>
2021-04-30 14:23:25 +0100

    add crud products

--- app/Http/Controllers/DishesController.php
+++ app/Http/Controllers/DishesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Producto;
 
 class DishesController extends Controller
 {
@@ -13,7 +14,8 @@ class DishesController extends Controller
      */
     public function index()
     {
-        return view('dishes.index');
+        $dishes = Producto::all();
+        return view('dishes.index', compact('dishes'));
     }
 
     /**
@@ -23,7 +25,7 @@ class DishesController extends Controller
      */
     public function create()
     {
-        //
+        return view('dishes.create');
     }
 
     /**
@@ -34,7 +36,26 @@ class DishesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = request()->validate([
+            'name' => ['required', 'min:2', 'max:50'],
+            'category' => ['required', 'min:2', 'max:50'],
+            'ingredients' => ['required', 'min:2', 'max:100'],
+            'price' => ['required', 'min:1'],
+            'image' => 'required',
+            ], [
+            'name.required' => 'El campo nombre es obligatorio',
+            'name.min' => 'El nombre debe tener mínimo 2 caracteres',
+            'name.max' => 'El nombre debe tener máximo 50 caracteres',
+            'category.required' => 'El campo categoria es obligatorio',
+            'category.min' => 'El campo categoria debe tener mínimo 2 caracteres',
+            'category.max' => 'El campo categoria debe tener máximo 50 caracteres',
+            'ingredients.required' => 'El campo categoria es obligatorio',
+            'ingredients.min' => 'El campo ingredientes debe tener mínimo 2 caracteres',
+            'ingredients.max' => 'El campo ingredientes debe tener máximo 100 caracteres',
+            'image.required' => 'El campo imagen es obligatorio',
+        ]);
+        Producto::create($request->all());
+        return redirect()->route('dishes.index');
     }
 
     /**
@@ -45,7 +66,8 @@ class DishesController extends Controller
      */
     public function show($id)
     {
-        //
+        $dish = Producto::findOrFail($id);
+        return view('dishes.show', compact('dish'));
     }
 
     /**
@@ -56,7 +78,8 @@ class DishesController extends Controller
      */
     public function edit($id)
     {
-        //
+        $dish = Producto::findOrFail($id);
+        return view('dishes.edit', compact('dish'));
     }
 
     /**
@@ -68,7 +91,27 @@ class DishesController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $data = request()->validate([
+            'name' => ['required', 'min:2', 'max:50'],
+            'category' => ['required', 'min:2', 'max:50'],
+            'ingredients' => ['required', 'min:2', 'max:100'],
+            'price' => ['required', 'min:1'],
+            'image' => 'required',
+            ], [
+            'name.required' => 'El campo nombre es obligatorio',
+            'name.min' => 'El nombre debe tener mínimo 2 caracteres',
+            'name.max' => 'El nombre debe tener máximo 50 caracteres',
+            'category.required' => 'El campo categoria es obligatorio',
+            'category.min' => 'El campo categoria debe tener mínimo 2 caracteres',
+            'category.max' => 'El campo categoria debe tener máximo 50 caracteres',
+            'ingredients.required' => 'El campo categoria es obligatorio',
+            'ingredients.min' => 'El campo ingredientes debe tener mínimo 2 caracteres',
+            'ingredients.max' => 'El campo ingredientes debe tener máximo 100 caracteres',
+            'image.required' => 'El campo correo es obligatorio',
+        ]);
+        $dish = Producto::findOrFail($id);
+        $dish->update($request->all());
+        return redirect()->route('dishes.index');
     }
 
     /**
@@ -79,6 +122,10 @@ class DishesController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $dish = Producto::findOrFail($id);
+
+        $dish->delete();
+
+        return redirect()->route('dishes.index');
     }
 }
--- resources/views/dishes/index.blade.php
+++ resources/views/dishes/index.blade.php
@@ -1,18 +1,49 @@
 @extends('layout')
 
-@section('title', "Nuestra Carta")
+@section('title', "Nuestra carta")
 
 @section('content')
-  <div class="row">
-    <div class="col">
-      <h1>Carta del Restaurante</h1>
-    </div>
-  </div>
+    <h1 class="mb-3">Platos</h1>
 
-  <div class="row">
-    <div class="col">
-
-
-    </div>
-  </div>
+    @if ($dishes->isNotEmpty())
+        <table class="table">
+            <thead class="thead-dark">
+                <div class="form-group mt-2 mt-md-0 mb-3 row">
+                    <div class="col-10">  
+                        <a href="{{ route('dishes.create') }}" class="btn btn-primary">Nuevo plato</a>
+                    </div>
+                </div>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Categoria</th>
+                    <th scope="col">Ingredientes</th>
+                    <th scope="col">Precio</th>
+                    <th scope="col">Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+            @foreach($dishes as $dish)
+            <tr>
+                <th scope="row">{{ $dish->idProduct }}</th>
+                <td>{{ $dish->name }}</td>
+                <td>{{ $dish->category }}</td>
+                <td>{{ $dish->ingredients }}</td>
+                <td>{{ $dish->price }}</td>
+                <td>
+                    <form action="{{ route('dishes.destroy', [$dish->idProduct]) }}" method="POST">
+                        {{ method_field('DELETE') }}
+                        {{ csrf_field() }}
+                        <a href="{{ route('dishes.show', [$dish->idProduct]) }}" class="btn btn-info"><span class="oi oi-eye"></span></a> 
+                        <a href="{{ route('dishes.edit', [$dish->idProduct]) }}" class="btn btn-primary"><span class="oi oi-pencil"></span></a> 
+                        <button type="submit" class="btn btn-danger"><span class="oi oi-trash"></span></button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+            </tbody>
+        </table>
+    @else
+        <p>No hay platos en la carta</p>
+    @endif
 @endsection
