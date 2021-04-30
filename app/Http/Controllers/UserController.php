5a06be900cb7fdd01de1bcce4be53041a432bd20
Damon Garcia Garcia <damon99@hotmail.es>
2021-04-30 01:01:01 +0100

    Eliminar usuarios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -152,6 +152,10 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $user = User::findOrFail($id);
+
+        $user->delete();
+
+        return redirect()->route('users.index');
     }
 }
